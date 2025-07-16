d9fb2441bde4f65e640147ac6007e6fe5ac45caf
hannah winner <124088297+hannahisawinner@users.noreply.github.com>
2025-07-16 15:06:16 -0600

    Update script.js
    Adding script function for the FAQ dropdown

--- script.js
+++ script.js
@@ -30,3 +30,20 @@
       }
     });
 
+
+
+
+
+
+
+document.querySelectorAll('.faq-question').forEach(button => {
+  button.addEventListener('click', () => {
+    const item = button.closest('.faq-item');
+    item.classList.toggle('open');
+  });
+});
+
+
+
+
+
