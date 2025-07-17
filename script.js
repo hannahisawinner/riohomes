db0016bbe24d3c363e485abbbbe35f9cc2a91f15
hannah winner <124088297+hannahisawinner@users.noreply.github.com>
2025-07-16 19:22:07 -0600

    Update script.js
    Removing the FAQs script code (to then add it to a separate page within the faqs folder)

--- script.js
+++ script.js
@@ -36,12 +36,7 @@
 
 
 
-document.querySelectorAll('.faq-question').forEach(button => {
-  button.addEventListener('click', () => {
-    const item = button.closest('.faq-item');
-    item.classList.toggle('open');
-  });
-});
+
 
 
 
