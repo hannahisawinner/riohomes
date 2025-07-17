e48d9e26a0ffb98dcbaf334faaadb53bc70c5f32
hannah winner <124088297+hannahisawinner@users.noreply.github.com>
2025-07-16 19:22:39 -0600

    Update script.js
    Adding the answer dropdown functionality script

--- faqs/script.js
+++ faqs/script.js
@@ -1,1 +1,6 @@
-
+document.querySelectorAll('.faq-question').forEach(button => {
+  button.addEventListener('click', () => {
+    const item = button.closest('.faq-item');
+    item.classList.toggle('open');
+  });
+});
