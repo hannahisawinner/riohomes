199c401bb0dcd51ea987a5bdb13914d576f4a8f9
hannah winner <124088297+hannahisawinner@users.noreply.github.com>
2025-07-16 21:25:36 -0600

    Update script.js
    adding the script for the hamburger menu open and closing

--- script.js
+++ script.js
@@ -31,6 +31,16 @@
     });
 
 
+// Open/Close Navbar Hamburger Overlay Menu
+function openMenu() {
+    document.getElementById("overlayMenu").classList.add("active");
+    document.body.classList.add("menu-open");
+}
+
+function closeMenu() {
+    document.getElementById("overlayMenu").classList.remove("active");
+    document.body.classList.remove("menu-open");
+}
 
 
 
