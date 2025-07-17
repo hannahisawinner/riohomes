6db292ac34abd7f9c6afcd3b32e01fe69e855127
hannah winner <124088297+hannahisawinner@users.noreply.github.com>
2025-07-17 10:29:16 -0600

    Update script.js
    adding the focus field to the script

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-    const contactForm = document.getElementById('contact-form');
-    const responseEl = document.getElementById('form-response');
+const contactForm = document.getElementById('contact-form');
+const responseEl = document.getElementById('form-response');
 
-    contactForm.addEventListener('submit', async function(e) {
-      e.preventDefault();
+contactForm.addEventListener('submit', async function(e) {
+    e.preventDefault();
 
-      const formData = new FormData();
-      formData.append('formType', 'contact');
-      formData.append('name', contactForm.name.value);
-      formData.append('email', contactForm.email.value);
-      formData.append('phone', contactForm.phone.value);
-      formData.append('message', contactForm.message.value);
+const formData = new FormData();
+    formData.append('formType', 'contact');
+    formData.append('name', contactForm.name.value);
+    formData.append('email', contactForm.email.value);
+    formData.append('phone', contactForm.phone.value);
+    formData.append('focus', contactForm.focus.value);
+    formData.append('message', contactForm.message.value);
     
-      try {
+    try {
         const res = await fetch('https://script.google.com/macros/s/AKfycbyuATD6ZuwyKYMXO0yZpsJfBxeqdd2KwEEM11WgwxJKJ7-4NLlXLTwElkPFgMpzRj4J/exec', {
           method: 'POST',
           body: formData
@@ -24,11 +25,11 @@
         } else {
           responseEl.textContent = "❌ Something went wrong. Please try again.";
         }
-      } catch (error) {
+    } catch (error) {
         console.error(error);
         responseEl.textContent = "⚠️ Error submitting the form.";
-      }
-    });
+    }
+});
 
 
 // Open/Close Navbar Hamburger Overlay Menu
