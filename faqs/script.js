a50d219ac3aef3c8151393b6de0e0de3594d5ba8
hannah winner <124088297+hannahisawinner@users.noreply.github.com>
2025-07-16 21:57:53 -0600

    Update script.js
    adding the hamburger menu navbar script

--- faqs/script.js
+++ faqs/script.js
@@ -4,3 +4,14 @@ document.querySelectorAll('.faq-question').forEach(button => {
     item.classList.toggle('open');
   });
 });
+
+// Open/Close Navbar Hamburger Overlay Menu
+function openMenu() {
+    document.getElementById("overlayMenu").classList.add("active");
+    document.body.classList.add("menu-open");
+}
+
+function closeMenu() {
+    document.getElementById("overlayMenu").classList.remove("active");
+    document.body.classList.remove("menu-open");
+}
